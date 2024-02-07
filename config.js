5db056065af8078f1b50a951ec9c9cf4cbde64bd
FinalHD <65974064+FinalRX@users.noreply.github.com>
2024-02-07 13:33:40 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const Config = {
+    discord: {
+        token: "",
+        guildID: "",
+        broadcastingChannelId: "",
+        followerChannelId: "",
+        mods: []
+    },
+
+    twitch: {
+        username: "mutenjay",
+        clientId: "",
+        clientSecret: ""
+    },
+}
+
+module.exports = Config;
